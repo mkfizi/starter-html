669bb0cd49f3ab474eb72d66bbe9d0f15892ba91
Khafizi Noh <khafizi.noh@gmail.com>
2025-03-09 02:20:14 +0800

    Put viewport height fix as inline code

--- src/js/index.js
+++ src/js/index.js
@@ -1,46 +1,8 @@
 'use strict';
 
 (function () {
-
     const app = {};
 
     app.name = 'Starter HTML';
     app.version = '0.1.0';
-
-    app.ui = {
-        viewportHeight: {
-
-            // Workaround fix to handle viewport height issue on mobile browsers.
-            // https://stackoverflow.com/questions/37112218/css3-100vh-not-constant-in-mobile-browser
-            toggle: () => {
-                document.documentElement.style.setProperty('--vh', (window.innerHeight * 0.01) + 'px');
-            }
-        },
-
-        // Initialize view
-        init: () => {
-            app.ui.viewportHeight.toggle();
-        }
-    }
-
-    app.event = {
-        window: {
-            
-            // Handle window 'resize' event.
-            resize: () => {
-                app.ui.viewportHeight.toggle();
-            }
-        },
-
-        init: () => {
-            window.addEventListener('resize', app.event.window.resize);
-        }
-    },
-
-    app.init = () => {
-        app.ui.init();
-        app.event.init();
-    }
-
-    app.init();
 })();
